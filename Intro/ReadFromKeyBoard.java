eae1ed9967a001a92f7dbfff95fe70b2e7db059d
rohya-28 <rohitarsude28@gmail.com>
2023-01-01 19:03:26 -0400

    first Java Program

---
+++ Intro/ReadFromKeyBoard.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ReadFromKeyBoard {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        float x = sc.nextFloat();
+
+        System.out.println(x);
+    }
+}
