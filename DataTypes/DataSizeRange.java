15711d9d78e2caff66f203eb9dac7b99d1f83deb
rohya-28 <rohitarsude28@gmail.com>
2023-01-01 21:01:58 -0400

    Byte Data Type Range

--- DataTypes/DataSizeRange.java
+++ DataTypes/DataSizeRange.java
@@ -3,9 +3,9 @@ package DataTypes;
 public class DataSizeRange {
     public static void main(String args[]) 
     {
-        System.out.println("Int Min:" +Integer.MIN_VALUE);
-        System.out.println("Int Max:" +Integer.MAX_VALUE);
-        System.out.println("Int Bytes:" +Integer.BYTES);
+        System.out.println("Byte Min:" +Byte.MIN_VALUE);
+        System.out.println("Byte Max:" +Byte.MAX_VALUE);
+        System.out.println("Bytes Size:" +Byte.SIZE);
 
     }
 }
