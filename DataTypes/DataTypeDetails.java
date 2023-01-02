6dc27bcdb3731f35ad5cdfa329660d82b7a2ba1b
rohya-28 <rohitarsude28@gmail.com>
2023-01-01 22:22:09 -0400

    Int To Byte

---
+++ DataTypes/DataTypeDetails.java
@@ -0,0 +1,12 @@
+package DataTypes;
+
+public class DataTypeDetails {
+    
+    public static void main(String args[])
+    {
+        //Data Type Conversation
+        int x = 5;
+
+        System.out.println(Integer.toBinaryString(x));
+    }
+}
