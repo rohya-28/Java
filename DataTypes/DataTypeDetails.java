7537d84dbed394eaa0d1b77d633e7b8bbcc69d4e
rohya-28 <rohitarsude28@gmail.com>
2023-01-01 22:37:41 -0400

    Data types

--- DataTypes/DataTypeDetails.java
+++ DataTypes/DataTypeDetails.java
@@ -5,6 +5,8 @@ public class DataTypeDetails {
     public static void main(String args[])
     {
         //Data Type Conversation
+        // Float is available up to 6 decimal points like 12.251615 
+        // double is available up to 15 decimal points like 12.25161525161512 
         int x = 5;
 
         System.out.println(Integer.toBinaryString(x));
