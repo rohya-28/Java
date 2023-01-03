d3eeb293c5d931664f35d434a686e8e9d121c804
rohya-28 <rohitarsude28@gmail.com>
2023-01-03 19:47:08 -0400

    left shifted

---
+++ Oprators/bitWiseDemo.java
@@ -0,0 +1,12 @@
+public class bitWiseDemo {
+    public static void main(String args[])
+    {
+        int x=10 , y;
+
+        y=x<<6;
+
+        // Here if you left shifted  X by N than it will multiply by 2 power N
+
+        System.out.println(y);
+    }
+}
