c8c181ac921a71cc75ddc88fe75b7e7ef2a0dda7
rohya-28 <rohitarsude28@gmail.com>
2023-01-03 19:52:13 -0400

    Right and left shift

--- Oprators/bitWiseDemo.java
+++ Oprators/bitWiseDemo.java
@@ -3,10 +3,23 @@ public class bitWiseDemo {
     {
         int x=10 , y;
 
-        y=x<<6;
+        y=x<<2;
 
         // Here if you left shifted  X by N than it will multiply by 2 power N
 
         System.out.println(y);
+
+
+        /*--------------------------*/
+
+        int a=10;
+
+        float b;
+
+        b=a>>2;
+
+        // Here if you right shifted A by N than it will divide by 2 power of N
+
+        System.out.println(b);
     }
 }
