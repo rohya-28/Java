610b116966d5a9c5c555821b0a5c11cb7672fe06
rohya-28 <rohitarsude28@gmail.com>
2023-01-03 13:40:47 -0400

    Area and Volume Of Cuboid

---
+++ Oprators/Cuboid.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Cuboid {
+    public static void main(String args[])
+    {
+        int height, length, base;
+        float area , volume;
+
+        System.out.println("Enter The Height Length Base of Cuboid");
+        Scanner input = new Scanner(System.in);
+
+        height = input.nextInt();
+        length = input.nextInt();
+        base = input.nextInt();
+
+        area=(length*base)+(base*height)+(length*height);
+        volume=length*base*height;
+
+
+        System.out.println("Volume Of Cuboid Is " +volume);
+        System.out.println("Area Of Cuboid Is " +area);
+
+
+    }
+}
